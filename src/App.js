d697ed242e774b40c33849b2f0de8eed71d45bc9
Gary J <github@accu.ca>
2022-05-22 11:54:18 -0700

    Create App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {SafeAreaView, StatusBar, } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
