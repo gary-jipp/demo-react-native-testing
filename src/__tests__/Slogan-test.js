d9d2bc43a5a0a2cd0318f83056bc2652bcbd737c
Gary J <github@accu.ca>
2022-05-23 21:33:49 -0700

    Update

---
+++ src/__tests__/Slogan-test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slogan from 'component/Slogan';
+// import App from 'App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+// Use built-in react test renderer
+it('Slogan button sets State', () => {
+  const tree = renderer.create(<Slogan text="Today is the day!" />);
+
+});
