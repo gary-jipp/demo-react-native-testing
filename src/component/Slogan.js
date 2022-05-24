417a87170697c1ae7c96df24a6a51750d8c329de
Gary J <github@accu.ca>
2022-05-23 21:28:22 -0700

    Update

--- src/component/Slogan.js
+++ src/component/Slogan.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Pressable, Button, } from 'react-native';
+import {StyleSheet, Text, View, Button, } from 'react-native';
 
 export default Slogan = (props) => {
   const [text, setText] = useState('');
