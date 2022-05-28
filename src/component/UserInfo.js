5f36dbffd774adec617a371d8516b76bffafe5d2
Gary J <github@accu.ca>
2022-05-27 21:50:19 -0700

    Update

--- src/component/UserInfo.js
+++ src/component/UserInfo.js
@@ -2,8 +2,6 @@ import React from "react";
 import {StyleSheet, Text, View} from "react-native";
 import Slogan from "./Slogan";
 
-
-
 export default UserInfo = (props) => {
   return (
     <View style={styles.container} accessibilityLabel="Info" >
